b29a062294dd0095812f41d4733e3102b9bacc4c
Shozen Dan <xiaoran.duan@gmail.com>
2021-07-12 11:30:37 +0900

    adding distribution plots

--- src/analysis/flow.jl
+++ src/analysis/flow.jl
@@ -4,7 +4,7 @@ using CSV, DataFrames, Gadfly, Statistics, DataFramesMeta, Impute, Dates
 include("../visualization.jl")
 include("../utilities.jl")
 
-df = CSV.read("data/analytical/cows-analytic.csv", DataFrame)
+df = CSV.read("../../data/analytical/cows-analytic.csv", DataFrame)
 
 df.mdurS = Second.(df.tend - df.tbegin)
 df.mdurM = round.(df.mdurS, Dates.Minute) .|> Dates.value
@@ -21,7 +21,8 @@ df2 = @where(df, :ypmrr .!= ismissing(:ypmrr))
 
 plot(df2,
   x = :ypmlf,
-  Geom.histogram
+  Geom.histogram,
+  Guide.xticks(ticks = [0,1,2,3,4,5,10,20])
 )
 
 ismissing.(df.ypmrr) |> sum
