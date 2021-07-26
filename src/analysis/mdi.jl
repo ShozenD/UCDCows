147208dcdea9679afce9ff5efe9f81cc0215da11
Shozen Dan <xiaoran.duan@gmail.com>
2021-07-26 15:21:41 +0900

    updates

--- src/analysis/mdi.jl
+++ src/analysis/mdi.jl
@@ -10,12 +10,12 @@ lactnum_summary = @by(df, :lactnum, obs = length(:id)) |> x -> sort(x, :lactnum)
 df2 = @where(df, :lactnum .<= 2)
 
 # Find cows with abnormal MDi values 
-df_abnormal_mdi = @where(df_subset, :mdi .> 1.4)
+df_abnormal_mdi = @where(df2, :mdi .> 1.4)
 id_abnormal_mdi = unique(df_abnormal_mdi.id)
 
 # Filter data for cows with abnormal MDi
-df_subset.abnormal_mdi_flag = [id in id_abnormal_mdi for id in df_subset.id]
-df_abnormal_cows = @where(df_subset, :abnormal_mdi_flag .== 1)
+df2.abnormal_mdi_flag = [id in id_abnormal_mdi for id in df2.id]
+df_abnormal_cows = @where(df2, :abnormal_mdi_flag .== 1)
 abnorm_cow_id = unique(df_abnormal_cows.id)
 
 # Plot conductivity trends
@@ -69,13 +69,13 @@ function mctyield(D::DataFrame)
 end
 
 df3 = mctyield(df2)
-df3 = @where(df3, :mdi .!= ismissing(:mdi))
+df2 = @where(df2, :mdi .!= ismissing(:mdi))
 
 df3.nonmctyield
 df3.mctyield
 
-plot(df3, x = :mdi, y =:mctyield, Geom.boxplot)
-plot(df3, x = :mdi, y =:nonmctyield, Geom.boxplot)
+plot(df2, x = :mdi, y =:mctyield, Geom.boxplot)
+plot(df2, x = :mdi, y =:nonmctyield, Geom.boxplot)
 
 df4 = DataFrame([
   :type => [repeat(["max cond teat"], nrow(df3)); repeat(["non max cond teats"], nrow(df3))],
@@ -87,4 +87,67 @@ plot(df4,
   x = :mdi, y =:yield, 
   color =:type, 
   Geom.boxplot
+)
+
+using GLM
+
+m1 = lm(@formula(mdi ~ condmax), df2)
+
+# Get Residual Sum of Squares Σ(yᵢ - f(xᵢ))²
+rss = deviance(m1)
+mse = rss / nrow(df2)
+
+m2 = lm(@formula(mdi ~ condmax + lastmilkint), df2)
+rss = deviance(m2)
+mse = rss / nrow(df2)
+
+m3 = lm(@formula(mdi ~ condmax + lastmilkint + bloodtot), df2)
+rss = deviance(m3)
+mse = rss / nrow(df2)
+
+m4 = lm(@formula(mdi ~ condmax * lastmilkint * bloodtot), df2)
+rss = deviance(m4)
+mse = rss / nrow(df2)
+
+using MLJ, Statistics, LinearAlgebra
+
+train, test = partition(eachindex(df2.mdi), 0.7, shuffle = true)
+
+df_train, df_test = df2[train,:], df2[test,:]
+
+y_train, X_train = df_train.mdi, df_train[:,[:condmax, :lastmilkint, :bloodtot]]
+y_test, X_test = df_test.mdi, df_test[:,[:condmax, :lastmilkint, :bloodtot]]
+
+ols = lm(@formula(mdi ~ condmax * lastmilkint * bloodtot), df2)
+ŷ = GLM.predict(ols, X_test)
+ŷ = round.(ŷ; digits = 1)
+
+mean(skipmissing((y_test - ŷ).^2))
+mean(skipmissing(abs.(y_test - ŷ)))
+
+dfres = DataFrame([
+  :y => y_test,
+  :yhat => ŷ
+])
+
+set_default_plot_size(16cm, 10cm)
+
+df3 = @where(df2, :mdi .!= ismissing(:mdi))
+plot(df3,
+  x = :mdi, y = :condmax,
+  Geom.boxplot,
+  Guide.xticks(ticks=[1,1.4,2,3,4,5,10])
+)
+
+plot(df3,
+  x = :mdi, y = :yieldmin,
+  Geom.boxplot,
+  Guide.xticks(ticks=[1,1.4,2,3,4,5,10])
+)
+
+plot(dfres,
+  x = :y, y = :yhat,
+  Geom.point,
+  size = [0.5mm],
+  Guide.xticks(ticks=[1,1.4,2,3,4,5,10])
 )
